70431b66008efa21d5db4175ee94ccabfabe059d
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-30 01:06:50 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -17,7 +17,5 @@ function divSelector() {
 }
 
 function firstListItem() {
-  $('parent-tag child-tag:first-child')$('ul li')$('#pic-list')
-  $('ul li')
-  $('#pic-list')
+  return $('parent-tag child-tag:first-child')$('ul li')$('#pic-list')
 }
